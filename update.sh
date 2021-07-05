2d0924809cda73be47207b9991a7228ceca72c21
Shashank Sinha <shashank@crowdbotics.com>
2021-07-06 01:06:21 +0400

    Heroku (#13)
    * add build step
    
    * add build step
    
    * add apache config
    
    * add run step
    
    * update command
    
    * add release command
    
    * add release command
    
    * update command
    
    * update command
    
    * add script
    
    * add script

--- update.sh
+++ update.sh
@@ -1,2 +1,9 @@
-sed -i "s/80/$PORT/g" /etc/apache2/sites-enabled/000-default.conf /etc/apache2/ports.conf
-service apache2 start
+#!/usr/bin/env bash
+
+#sed -i "s/80/$PORT/g" /etc/apache2/sites-enabled/000-default.conf /etc/apache2/ports.conf
+#service apache2 start
+
+sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
+sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/apache2.conf
+sed -i "s/VirtualHost \*:80/VirtualHost \*:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
+apache2-foreground "$@"
